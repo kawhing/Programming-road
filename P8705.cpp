dbb4f1873c7906b19394de6ef194ecd2da5ce48b
kawhing <kawhing@users.noreply.github.com>
2025-03-09 23:11:24 +0800

    添加 P8705 题目的初始代码框架

---
+++ P8705.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void question(){
+    char c;
+    cin >> c;
+    switch(c){
+        case 'A':
+            questionA();
+            break;
+        case 'B':
+            questionB();
+            break;
+        case 'C':
+            questionC();
+            break;
+        case 'D':
+            questionD();
+            break;
+        case 'E':
+            questionE();
+            break;
+        default:
+            return;
+    }
+}
+
+void questionA(){
+    int physical_strength = 10000, loss = 600, add = 300;
+    
+}
+
+void questionB(){
+
+}
+
+void questionC(){
+
+}
+
+void questionD(){
+
+}
+
+void questionE(){
+
+}
+
+int main(){
+    question();
+    return 0;
+}
