f4a7a90671a3da0959beeca7064ecba157399742
kawhing <kawhing@users.noreply.github.com>
2025-04-10 22:08:13 +0800

    优化洛谷U535982题解

--- luogu/U535982.cpp
+++ luogu/U535982.cpp
@@ -1,53 +1,32 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-//U535982 J-A 小梦的AB交换 通过6 得分30
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll minOperate(string s, ll sSize){
-	int count = 0;
-	for(ll i = 1; i < sSize-1; ++i){
-		if(s[i] == s[i-1]){
-			if(s[i] == s[i+1]){
-				for(ll j = i+2; j < sSize; ++j){
-					if(s[j] != s[j-1]) continue;
-					else if(s[j] == s[j+1]){
-						if(s[j] != s[j] && s[j] == s[j+1]){
-							swap(s[j], s[i]);
-							++count;
-						}
-					}
-				}
-			}else if(s[i] != s[i+1]){
-				swap(s[i], s[i+1]);
-				++count;	
-			}
-		}else if(s[i] != s[i-1]){
-			continue;
-		}
-	}
-	return count;
-}
-void print(vector<ll> result){
-	for(ll i = 0; i < result.size(); ++i){
-		cout << result[i] << endl;
-	}
-}
-void test(){
-	ll t;
-	cin >> t;// 组数
-	vector<ll> result;
-	for(ll i = 0; i < t; ++i){
-		ll n;// s长度的一半 
-		cin >> n;
-		string s;
-		cin >> s;
-		result.push_back(minOperate(s, n*2));
-	}
-	print(result);  
-}
+ll t, n;
 int main() {
-	test();
+	cin >> t;
+	while(t--){
+		string s;
+		cin >> n;
+		cin >> s;
+		ll noPatch = 0, minNoPatch = 0;
+		for(ll i = 0; i < 2*n; ++i){
+			if(i%2 == 0){
+				if(s[i] != 'A') ++noPatch;
+			}else{
+				if(s[i] != 'B') ++noPatch;
+			}
+		}
+		minNoPatch = noPatch;
+		noPatch = 0;
+		for(ll i = 0; i < 2*n; ++i){
+			if(i%2 == 0){
+				if(s[i] != 'B') ++noPatch;
+			}else{
+				if(s[i] != 'A') ++noPatch;
+			}
+		}
+		minNoPatch = min(minNoPatch, noPatch);
+		cout << minNoPatch/2 << endl;
+	}
 	return 0;
 }
