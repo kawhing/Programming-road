8ad484783810ab352910fef8aadaa3a594cc363e
kawhing <kawhing@users.noreply.github.com>
2025-05-19 19:15:17 +0800

    添加洛谷P4413题解,平均数问题

---
+++ solutions/luogu/P4413.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void solve(){
+    ll R1, S;
+    cin >> R1 >> S;
+    cout << 2*S-R1 << endl;
+}
+int main(){
+    solve();
+    return 0;
+}
