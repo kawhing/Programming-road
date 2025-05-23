a5fed9788f82bcd9d64cf87f4010574be24de54b
kawhing <kawhing@users.noreply.github.com>
2025-05-23 21:39:08 +0800

    补充函数对象的另外两个性质

--- learning/cpp/container/functors/functors.cpp
+++ learning/cpp/container/functors/functors.cpp
@@ -15,14 +15,32 @@ void test01(){
 //2.函数对象超出普通函数的概念，函数对象可以有自己的状态
 class MyPrint{
     public:
+        MyPrint(){
+            this->count = 0;
+        }
         void operator()(string test){
             cout << test << endl;
+            ++this->count;
         }
+        int count;//内部自己状态
 };
 void test02(){
     MyPrint myPrint;
     myPrint("Hello World!");
+    myPrint("Hello World!");
+    myPrint("Hello World!");
+    myPrint("Hello World!");
+    cout << "myPrint调用次数为:" << myPrint.count << endl;
+}
+//3.函数对象可以作为参数传递
+void doPrint(MyPrint &mp, string test){
+    mp(test);
+}
+void test03(){
+    MyPrint myprint;
+    doPrint(myprint, "Hello World!");
 }
 int main(){
-    test02();
+    test03();
+    return 0;
 }
