94e9ce06b8cf776f6c146388d2c2cd37acc7cfab
kawhing <kawhing@users.noreply.github.com>
2025-05-27 19:32:23 +0800

    利用到getline函数解决洛谷P1765

--- solutions/luogu/P1765.cpp
+++ solutions/luogu/P1765.cpp
@@ -1,14 +1,30 @@
-#include<iostream>
-#include<string>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main(){
-    int count = 0;
-    string str;
-    cin >> str;
-    for(int i = 0; i < str.size(); ++i){
-        
+typedef long long ll;
+void solve(){
+    string line;
+    ll ans = 0;
+    getline(cin, line);
+    for(char c : line){
+        if(c == ' '){
+            ++ans;
+        }else{
+            if(c <= 'o'){
+                int temp = (int(c-'a')+1) % 3;
+                if(temp == 0) ans += 3;
+                else ans += temp;
+            }else if(c >= 'p' && c <= 's'){
+                ans += c-'p'+1;
+            }else if(c >= 't' && c <= 'v'){
+                ans += c-'t'+1;
+            }else{
+                ans += c-'w'+1;
+            }
+        }
     }
-    cout << count << endl;
+    cout << ans << endl;
+}
+int main(){
+    solve();
     return 0;
 }
