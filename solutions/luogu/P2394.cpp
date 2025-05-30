276e0dd458f38d6d32851cd3cddb9baddc9df2c5
kawhing <kawhing@users.noreply.github.com>
2025-05-30 22:38:30 +0800

    添加洛谷解决方案：问题P2394

---
+++ solutions/luogu/P2394.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    double x;
+    cin >> x;
+    cout << fixed << setprecision(8) << x/23 << endl;
+}
+int main(){
+    solve();
+    return 0;
+}
