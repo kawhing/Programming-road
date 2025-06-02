4ef88669d15f7c90fc25f22f9107c5411a082144
kawhing <kawhing@users.noreply.github.com>
2025-06-02 21:19:46 +0800

    添加黑马程序员-演讲比赛流程管理系统-菜单界面

---
+++ learning/cpp/STL/speechContestProcessManagementSystem/include/manage.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+//设计管理类
+class Manage{
+    public:
+        //构造函数
+        Manage();
+        //展示菜单
+        void showMenu();
+        //析构函数
+        ~Manage();
+};
---
+++ learning/cpp/STL/speechContestProcessManagementSystem/src/manage.cpp
@@ -0,0 +1,16 @@
+#include "../include/manage.h"
+using namespace std;
+Manage::Manage(){
+
+}
+void Manage::showMenu(){
+    cout << "***************************************" << endl;
+    cout << "************ 欢迎参加演讲比赛 ***********" << endl;
+    cout << "************ 1.开始演讲比赛 ************" << endl;
+    cout << "************ 2.查看往届记录 ************" << endl;
+    cout << "************ 3.清空比赛记录 ************" << endl;
+    cout << "************ 0.退出比赛程序 ************" << endl;
+}
+Manage::~Manage(){
+
+}
