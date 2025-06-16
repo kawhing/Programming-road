5ab766e819baadc9e19ab545bbfb04da36b1f0a5
kawhing <kawhing@users.noreply.github.com>
2025-06-16 10:54:02 +0800

    feat: 完成 洛谷P5681 [CSP-J2019 江西] 面积

---
+++ solutions/luogu/P5681.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+	long long a, b, c;
+	cin >> a >> b >> c;
+	if(a*a > b*c) cout << "Alice" << endl;
+	else cout << "Bob" << endl;
+}
+int main(){
+	solve();
+	return 0;
+}
