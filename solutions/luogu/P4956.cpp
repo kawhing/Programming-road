0a4c8f62b249dfe39bf555814843099108c21e4e
kawhing <kawhing@users.noreply.github.com>
2025-07-02 16:09:47 +0800

    fix: 洛谷P4956 [COCI 2017/2018 #6] Davor

--- solutions/luogu/P4956.cpp
+++ solutions/luogu/P4956.cpp
@@ -1,22 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-void solove(){
-    ll n, fullSum = 0, getSum = 0, lossSum = 0;
+bool compare(vector<int> &a, vector<int> b){
+    if(a[0] > b[0]) return true;
+    else if(a[0] == b[0] && a[1] < b[1]) return true;
+    return false;
+}
+void solve(){
+    int n, x, k;
     cin >> n;
-    vector<ll> a(n), b(n);
-    for(ll i = 0; i < n; ++i){
-        cin >> a[i];
-        fullSum += a[i];
+    vector<vector<int>> ans;
+    for(x = 1; x <= 100;++x){
+        for(k = 1; ; ++k){
+            int sum = 0;
+            for(int i = 0; i < 7; ++i) sum += x + i * k;
+            sum *= 52;
+            if(sum == n){
+                ans.push_back({x, k});
+            }else if(sum > n){
+                break;
+            }
+        }
     }
-    for(ll i = 0; i < n; ++i){
-        cin >> b[i];
-        getSum += b[i];
-        lossSum += a[i]-b[i];
-    }
-    cout << fixed << setprecision(6) << (fullSum*3-getSum*2)*1.0/lossSum << endl; 
+    sort(ans.begin(), ans.end(), compare);
+    cout << ans[0][0] << endl << ans[0][1];
+    
 }
 int main(){
-    solove();
+    solve();
     return 0;
 }
