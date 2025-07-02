0ccf04bcd0d08d80bb554257d68a750ac04a571c
kawhing <kawhing@users.noreply.github.com>
2025-07-02 11:37:57 +0800

    fix: 洛谷B2108 图像模糊处理

--- solutions/luogu/B2108.cpp
+++ solutions/luogu/B2108.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using  namespace std;
 typedef long long ll;
-//部分通过
 void solve(vector<vector<ll>> &nums, ll n, ll m){
     vector<vector<ll>> temp = nums;
     for(ll i = 1; i < n-1; ++i){
         for(ll j = 1; j < m-1; ++j){
-            nums[i][j] += temp[i-1][j] + temp[i+1][j] + temp[i][j-1] + temp[i][j+1];
-            nums[i][j] /= 5;
+            ll sum = temp[i][j] + temp[i-1][j] + temp[i+1][j] + temp[i][j-1] + temp[i][j+1];
+            nums[i][j] = (sum + 2) / 5;
         }
     }
-    for(ll i = 1; i < n-1; ++i){
-        for(ll j = 1; j < m-1; ++j){
-            cout << nums[i][j] << " ";
+    for(ll i = 0; i < n; ++i){
+        for(ll j = 0; j < m; ++j){
+            cout << nums[i][j];
+            if(j < m-1) cout << " ";
         }
         cout << endl;
     }
