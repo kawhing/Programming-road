36d6d5ed4fff6acddda068e621ab63a0314d7d2b
kawhing <kawhing@users.noreply.github.com>
2025-07-02 22:27:09 +0800

    fix: 洛谷P1047 [NOIP 2005 普及组] 校门外的树

--- solutions/luogu/P1047.cpp
+++ solutions/luogu/P1047.cpp
@@ -1,35 +1,23 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-void  chonghe(int *test, int m, int *Chonghe){
-    for(int i = 0; i < m-1; ++i){
-        for(int j = i+1; j < m; ++j){
-            if(test[i][0] > test[j][0] && test[i][0] < test[j][1]){
-                chonghe[i] = true;
-            }
-            if(test[i][1] > test[j][0] && test[i][1] < test[j][1]){
-                chonghe[i] = true;
-            }
-            if(test[j][0] > test[i][0] && test[j][0] < test[i][1]){
-                chonghe[i] = true;
-            }
-            if(test[j][1] > test[i][0] && test[j][1] < test[i][1]){
-                chonghe[i] = true;
-            }
-        }
-    }
-}
-void ischonghe
-int main(){
-    int l, m, trees;//l:路长 m:区域的数量 trees:树的数量
-    cin >> l >> m;
-    bool isChonghe[m+1];
-    int test[m][2];
-    chonghe(test, m, isChonghe);
-    trees = l+1;
-    for(int i = 0; i < m; ++i){
-        int start, end;
-        cin >> test[i][0] >> test[i][1];
-    }
 
+struct zone{
+    int u, v;
+};
+void solve(){
+    int l, m, ans = 0;
+    cin >> l >> m;
+    vector<bool> line(l+1, true);
+    vector<zone> zones(m);
+    for(int i = 0; i < m; ++i){
+        cin >> zones[i].u >> zones[i].v;
+        for(int j = zones[i].u; j <= zones[i].v; ++j) line[j] = false;
+    }
+    for(bool i : line)
+        if(i) ++ans;
+    cout << ans << endl;
+}
+int main(){
+    solve();
     return 0;
 }
