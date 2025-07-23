6bb731ab462f41ba7fbd9f9c1766cd51ea848679
kawhing <kawhing@users.noreply.github.com>
2025-07-24 00:56:42 +0800

    fix: 修复洛谷 P12133 [蓝桥杯 2025 省 B] 产值调整 - 50轮基本已经稳定不需要完全要k轮

--- solutions/luogu/P12133.cpp
+++ solutions/luogu/P12133.cpp
@@ -1,21 +1,26 @@
 #include <bits/stdc++.h> 
 using namespace std;
 typedef long long ll;
-//目前时间复杂高，洛谷上也没有人发完美题解
+
+void solve(){
+    ll t, a, b, c, k;
+    cin >> t;
+    while(t--){
+        cin >> a >> b >> c >> k;
+        for(int i = 0; i < min(k, 50LL); ++i){
+            ll temp_a = (b+c)/2, temp_b = (a+c)/2, temp_c = (a+b)/2;
+            if(temp_a == a && temp_b == b && temp_c == c) {
+                break;
+            }
+            
+            a = temp_a, b = temp_b, c = temp_c;
+        }
+        
+        cout << a << " " << b << " " << c << endl;
+    }
+}
+
 int main(){
-	ll t;
-	cin >> t;
-	while(t--){
-		ll a, b, c, k;
-		cin >> a >> b >> c >> k;
-		for(ll i = 0; i < k; ++i){
-			//ll tmp1 = a, tmp2 = b, tmp3 = c;
-			ll tmp1 = (b+c)/2;
-			ll tmp2 = (a+c)/2;
-			ll tmp3 = (a+b)/2;
-			a = tmp1, b = tmp2, c = tmp3;
-		}
-		cout << a << " " << b << " " << c << endl;
-	}	
-	return 0;
+    solve();
+    return 0;
 }
