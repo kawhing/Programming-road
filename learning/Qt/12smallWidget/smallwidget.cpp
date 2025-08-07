0f26633ff45c8cd60765ba9114de4f2ab00d0993
kawhing <kawhing@users.noreply.github.com>
2025-08-07 21:52:39 +0800

    feat(learning): 初学Qt自定义组件

---
+++ learning/Qt/12smallWidget/smallwidget.cpp
@@ -0,0 +1,24 @@
+#include "smallwidget.h"
+#include "ui_smallwidget.h"
+
+SmallWidget::SmallWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::SmallWidget)
+{
+    ui->setupUi(this);
+    void(QSpinBox:: *spSignal)(int) = &QSpinBox::valueChanged;
+    void(QSlider:: *setVSignal)(int) = &QSlider::valueChanged;
+    connect(ui->spinBox, spSignal, ui->horizontalSlider, &QSlider::setValue);
+    connect(ui->horizontalSlider, setVSignal, ui->spinBox, &QSpinBox::setValue);
+
+}
+void SmallWidget::setNum(int num){
+    ui->spinBox->setValue(num);
+}
+int SmallWidget::getNum(){
+    return ui->spinBox->value();
+}
+SmallWidget::~SmallWidget()
+{
+    delete ui;
+}
