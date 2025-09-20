f03e656498cd177def086c5b6df1a334bebc976c
kawhing <kawhing@users.noreply.github.com>
2025-09-20 22:55:12 +0800

    feat(solutions/luogu): 完成洛谷题解 P13892 [蓝桥杯 2023 省 C] 求和

---
+++ solutions/luogu/P13892.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    long long sum = 0;
+    for(long long i = 1; i <= 20230408; ++i)
+        sum += i;
+    cout << sum << endl;
+}
+int main(){
+    solve();
+    return 0;
+}
