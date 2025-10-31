1cd8604820e9c91e3a621dd1654071944b46bba8
kawhing <kawhing@users.noreply.github.com>
2025-10-31 08:43:13 +0800

    remove: 删除logistics

--- learning/Java/洁美智慧物流客服平台V1.0/logistics/api/src/main/java/com/example/api/service/impl/UserServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.example.api.service.impl;
-
-import com.example.api.service.UserService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserServiceImpl implements UserService {
-
-}
