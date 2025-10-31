9e50b9e4416dd663891b1c399b42c373d3bb21cb
kawhing <kawhing@users.noreply.github.com>
2025-11-01 07:57:42 +0800

    fix(solutions/luogu): 修复洛谷题解 P8781 [蓝桥杯 2022 省 B] 修剪灌木

--- solutions/luogu/P8781.cpp
+++ solutions/luogu/P8781.cpp
@@ -1,39 +1,14 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-// not rearly reslove
-void addHigh(vector<int>& array, vector<int>& max){
-    for(int i = 0; i < array.size(); ++i){
-        ++array[i];
-        if(array[i] > max[i]){
-            max[i] = array[i];
-        }
-    }
-}
 
-void test(){
-    int n;
+void solve(){
+    int n, ans = 0;
     cin >> n;
-    vector<int> array(n, 0);
-    vector<int> max(n, 0);
-    int dir = 1;
-    int index = 0;
-    for(int day = 0; day < 2*(n-1)*n; ++day){
-        addHigh(array, max);
-        array[index] = 0;
-        index += dir;
-        if(index == n-1 || index == 0)
-            dir = -dir;
-        
+    for(int i = 1; i <= n; ++i){
+        cout << max(n-i, i-1) * 2 << endl;
     }
-
-    for(int i = 0; i < n; ++i){
-        cout << max[i] << endl;
-    }
-    
 }
-
 int main(){
-    test();
+    solve();
     return 0;
 }
