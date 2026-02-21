62c00752733a8a21b9c180dbba381b5a6458bd4b
kawhing <kawhing@users.noreply.github.com>
2026-02-21 17:19:37 +0800

    feat(solutions/leetcode): 1. 两数之和 优化版本\ - 采用哈希表降低时间复杂度到O(N)

--- solutions/leetcode/two-sum.cpp
+++ solutions/leetcode/two-sum.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int numsSize = nums.size();
-        int ans[2];
-        for(int i = 0; i < numsSize - 1; ++i){
-            for(int j = i + 1; j < numsSize; ++j){
-                if(nums[i] + nums[j] == target){
-                    return {i, j};
-                }
-            }
+        map<int, int> mp;
+        for(int i = 0; i < numsSize; ++i){
+            map<int, int>::iterator it = mp.find(target - nums[i]);
+            if(it != mp.end())
+                return {it->second, i};
+            mp[nums[i]] = i;
         }
-        return {ans[0], ans[2]};
+        return {};
     }
 };
