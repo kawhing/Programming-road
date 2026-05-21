0a4477fcc30a4895554e9c24e3606ab5a918e047
kawhing <kawhing@users.noreply.github.com>
2026-05-21 19:50:44 +0800

    fix(leetcode):  Fix LeetCode Adventure Mode: Stack, Q1. "build-an-array-with-stack-operations". Optimize the algorithm to reduce time complexity.

--- solutions/leetcode/Adventure_Mode/stack/build-an-array-with-stack-operations.cpp
+++ solutions/leetcode/Adventure_Mode/stack/build-an-array-with-stack-operations.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
-    bool isEqualTarget(vector<int>& target, stack<int> nums){
-        if(target.size() != nums.size()) return false;
-        for(int i = target.size() -1 ; i >= 0 ; --i){
-            if(target[i] != nums.top()){
-                return false;
-            }
-            nums.pop();
-        }
-        return true;
-    }
     vector<string> buildArray(vector<int>& target, int n) {
         vector<string> ans;
-        stack<int> nums;
         int pos = 0;
-        for(int i = 0; pos < target.size() && i < n; ++i){
-            nums.push(i + 1);
+        for (int i = 1; i <= n && pos < target.size(); ++i) {
             ans.push_back("Push");
-            if(target[pos] == i + 1){
+
+            if (i == target[pos]) {
                 ++pos;
-            }else{
-                nums.pop();
-                ans.push_back("Pop");
+            } else {
+             ans.push_back("Pop");
             }
-            if(pos + 1 == target.size() && isEqualTarget(target, nums)) break;
         }
-        if(!isEqualTarget(target, nums)) return{};
         return ans;
     }
 };
